737c2b46a2eafb9f408b4c65edab1d2d4cb87c60
Chitanya Bharathi <bharathichitanya@gmail.com>
2026-07-31 21:12:35 +0800

    Remove Vitest include glob override to fix test matching on Linux runner

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/test/setup.js',
-    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}']
+    setupFiles: './src/test/setup.js'
   }
 })
